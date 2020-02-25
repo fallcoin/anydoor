88487b9b7f5003987cae366423773eddeef89f65
Wenxin <1340984125@qq.com>
2020-02-25 22:33:41 +0800

    完成功能部分

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const http = require('http')
 const conf = require('./config/defaultConfig')
 const chalk = require('chalk')
+const path = require('path')
+const route = require('./helper/route')
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello HTTP!')
+	const filePath = path.join(conf.root, req.url)
+	route(req, res, filePath)
 })
 
 server.listen(conf.port, conf.hostname, () => {
-    const address = `http://${conf.hostname}:${conf.port}`
-    console.log(`Server started at ${chalk.green(address)}`)
+	const address = `http://${conf.hostname}:${conf.port}`
+	console.log(`Server started at ${chalk.green(address)}`)
 })
--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,4 +1,5 @@
 module.exports = {
-    hostname: '127.0.0.1',
-    port: 8888
+	root: process.cwd(),	//用户当前执行的文件夹
+	hostname: '127.0.0.1',
+	port: 8888
 }
